4a634e819634ce4a964dcc9f77a52c07726e5a6c
vaibhav.jhingran <vaibhav.jhingran@go-jek.com>
2019-09-17 14:49:41 +0530

    Added apk to project
    * Fixed path to apk file in desired capabilities.

--- setup/desired_capabilites.py
+++ setup/desired_capabilites.py
@@ -1,7 +1,9 @@
+import os
+
 capabilities = {
             "platformName": "Android",
             "platformVersion": "6.0",
-            "app": "/Users/vaibhavjhingran/code/python/babbel/app-debug.apk",
+            "app": os.path.abspath("../apk/app-debug.apk"),
             "appPackage": "com.github.fgoncalves.qa",
             "appActivity": ".MainActivity",
             "deviceName": "Pixel"
